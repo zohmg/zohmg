eb2be65a6d300556ac079f610199ae803875e074
Fredrik Mollerstrand <fredrik@last.fm>
2009-04-07 12:05:56 +0000

    temporarily wrapping things inside a try-block to get rid of the AttributeErrors I've been seeing.

--- mappers/weblogs.py
+++ mappers/weblogs.py
@@ -1,17 +1,23 @@
 # user's mapper.
 def map(key, value):
+    import sys
     from lfm.data.parse import web
 
     try: log = web.Log(value)
-    except ValueError: return
+    except: return
     ua = web.UserAgent()
 
-    ts = log.timestamp.ymd()
-    dimensions = {'country'   : log.country(),
-                  'domain'    : log.domain,
-                  'useragent' : ua.classify(log.agent),
-                  'usertype'  : ("user", "anon")[log.userid == None]
-                  }
-    values = {'pageviews' : 1}
+    try: # been seeing too many AttributeErrors lately..
+        ts = log.timestamp.ymd()
+        dimensions = {'country'   : log.country(),
+                      'domain'    : log.domain,
+                      'useragent' : ua.classify(log.agent),
+                      'usertype'  : ("user", "anon")[log.userid == None]
+                      }
+        values = {'pageviews' : 1}
+
+    except AttributeError:
+        sys.stderr.write("AttributeError!\n")
+        return
     
     yield ts, dimensions, values
