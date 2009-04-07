9e6ef69be685925f05010d2b5ce9ef639286cdbd
Fredrik Mollerstrand <fredrik@last.fm>
2009-04-07 12:08:22 +0000

    removed dumbo.sumreducer from dumbo.run(); addded PYTHONPATH to run.dumbo.cluster; added usertype to one of the projections

--- scripts/import.py
+++ scripts/import.py
@@ -6,4 +6,7 @@ from zohmg import Config, Mapper, Reducer
 if __name__ == "__main__":
     import dumbo
     from usermapper import map as usermap
-    dumbo.run(Mapper(usermap), Reducer, dumbo.sumreducer)
+    dumbo.run(Mapper(usermap), Reducer)
+    # will want to use a combiner, too.
+    # tried dumbo.sumreducer() but it ran into memory errors, which was weird.
+
