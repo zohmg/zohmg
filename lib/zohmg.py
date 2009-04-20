9fe23556584b2ca5d3dd6923a17f038acc882aac
Per Andersson <per@last.fm>
2009-04-20 15:22:03 +0100

    added environment reader class.

--- lib/zohmg.py
+++ lib/zohmg.py
@@ -31,7 +31,28 @@ class Config(object):
     def projections(self):
         return self.config['projections']
 
-class Mapper:
+
+class Environment(object):
+    def __init__(self):
+        self.environ = {}
+        self.read_environ()
+
+    def get(self,key):
+        return self.environ[key]
+
+    def read_environ(self):
+        sys.path.append("") # add cwd
+        try:
+            env = __import__("config/environment")
+        except ImportError:
+            print >> sys.stderr,"E: Could not import config/environment.py, does it exist?"
+            exit(1)
+
+        for key in dir(env):
+            self.environ[key] = env.__dict__[key]
+
+
+class Mapper(object):
     def __init__(self, usermapper):
         self.usermapper = usermapper
         self.projections = Config().projections()
@@ -50,7 +71,7 @@ class Mapper:
                     value = units[u]
                     yield (ts, reduced, u), value
 
-class Reducer:
+class Reducer(object):
     def __init__(self):
         self.reduces = self.counters['reduces']
         self.config = Config()
