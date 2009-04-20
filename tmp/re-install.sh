4f99de534c0cd2eb246a802c0d785ba377ad0bc0
Fredrik Mollerstrand <fredrik@last.fm>
2009-04-20 19:32:56 +0100

    freshened re-install.sh

--- tmp/re-install.sh
+++ tmp/re-install.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
+
+# clean,
 sudo rm -v /usr/bin/zohmg /usr/lib/python2.5/site-packages/zohmg-*-py2.5.egg
-sudo python setup.py  --verbose install
+
+# reinstate.
+echo -n hmm..
+sudo python setup.py  --verbose install > tmp/last.reinstall.log
+echo ok.
