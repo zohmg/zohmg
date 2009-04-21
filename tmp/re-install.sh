1dfbbb211a9d3aef8a8a525496478f3ba56383a2
Per Andersson <per@last.fm>
2009-04-21 15:19:45 +0100

    updated to use install.py.

--- tmp/re-install.sh
+++ tmp/re-install.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # clean,
-sudo rm -v /usr/bin/zohmg /usr/lib/python2.5/site-packages/zohmg-*-py2.5.egg
+sudo rm -v /usr/bin/zohmg /usr/lib/python2.5/site-packages/zohmg-*-py2.5.egg /usr/lib/python2.5/site-packages/hbase-*-py2.5.egg /usr/lib/python2.5/site-packages/thrift-*-py2.5.egg
 
 # reinstate.
 echo -n hmm..
-sudo python setup.py  --verbose install > tmp/last.reinstall.log
+sudo python install.py  --verbose install > tmp/last.reinstall.log
 sudo rm -rf build dist zohmg.egg-info
 echo ok.
