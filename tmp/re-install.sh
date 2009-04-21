0400cfa9eadd0bd9081200cbd9045c46f427b5d8
Fredrik Mollerstrand <fredrik@last.fm>
2009-04-21 09:20:37 +0100

    re-install cleans up.

--- tmp/re-install.sh
+++ tmp/re-install.sh
@@ -6,4 +6,5 @@ sudo rm -v /usr/bin/zohmg /usr/lib/python2.5/site-packages/zohmg-*-py2.5.egg
 # reinstate.
 echo -n hmm..
 sudo python setup.py  --verbose install > tmp/last.reinstall.log
+sudo rm -rf build dist zohmg.egg-info
 echo ok.
