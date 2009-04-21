c287b35645170ac4ada2f05920db2df06f6add06
Per Andersson <per@last.fm>
2009-04-21 12:04:01 +0100

    process now uses zohmg.config.Environ.

--- zohmg/process.py
+++ zohmg/process.py
@@ -1,4 +1,4 @@
-from zohmg.config import Config
+from zohmg.config import Config, Environ
 import os
 
 class Process(object):
@@ -14,31 +14,42 @@ class Process(object):
                 ('outputformat', outputformat),
                 ('input', input),
                 ('output','/tmp/does-not-matter'),
-                ('file','lib/utils.py'),
-                ('file','lib/zohmg.py'),
-                ('file','lib/usermapper.py'),
-                ('file','config.yaml')
+                # Push zohmg egg.
+                ('libegg','/usr/lib/python2.5/site-packages/zohmg-0.0.1-py2.5.egg'),
                 ]
 
-        # read class path, attach.
-        cp = os.getenv("CLASSPATH")
-        if cp != None:
+        # read environment and attach.
+        env = Environ()
+
+        opts.append(('hadoop',env.get("HADOOP_HOME")))
+
+        cp = env.get("CLASSPATH")
+        if cp is not None:
             for jar in cp.split(':'):
                 opts.append(('file', jar))
+        else:
+            msg = "E: CLASSPATH in config/environment is empty."
+            fail(msg)
+
+
+        # pull everything in config and lib.
+        # TODO
+        print os.walk("lib")
+        print os.walk("config")
 
         # stringify arguments.
         opts_args = ' '.join("-%s '%s'" % (k, v) for (k, v) in opts)
-        more_args = ' '.join(for_dumbo)
+        more_args = ' '.join(for_dumbo) # TODO: is this necessary?
         dumboargs = "%s %s" % (opts_args, more_args)
         print "giving dumbo these args: " + dumboargs
 
         # link-magic for usermapper.
         usermapper = os.path.abspath(".")+"/lib/usermapper.py"
         if os.path.isfile(usermapper):
+            # SECURITY
             # TODO: need to be *very* certain we're not unlinking the wrong file.
             os.unlink(usermapper)
         os.symlink(mapper,usermapper)
 
         # dispatch.
-        # TODO: is this where we will source config/env.sh, then?
         os.system("dumbo start tmp/import.py " + dumboargs)
