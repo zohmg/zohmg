961b597a4ac58683a9c3e49395aab6575dc8a6f6
Per Andersson <per@last.fm>
2009-04-21 12:05:29 +0100

    merged Config and Environ into one file.

--- zohmg/config.py
+++ zohmg/config.py
@@ -1,5 +1,5 @@
 from zohmg.utils import fail
-
+import sys
 
 class Config(object):
     # TODO: multiple dataset files
@@ -31,3 +31,24 @@ class Config(object):
 
     def projections(self):
         return self.config['projections']
+
+
+
+class Environ(object):
+    def __init__(self):
+        self.environ = {}
+        self.read_environ()
+
+    def get(self,key):
+        return self.environ[key]
+
+    def read_environ(self):
+        sys.path.append("") # add cwd so we can import from it.
+        try:
+            env = __import__("config/environment")
+        except ImportError, ioe:
+            msg = "E: Could not import config/environment.py. %s" % ioe.strerror
+            fail(msg,ioe.errno)
+
+        for key in dir(env):
+            self.environ[key] = env.__dict__[key]
