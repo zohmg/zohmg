920e0de237d1f85a3218e91b5d570660360de9ac
Per Andersson <per@last.fm>
2009-04-21 11:22:52 +0100

    + shortened name to environ. + added use of zohmg.utils.fail().

--- zohmg/environment.py
+++ zohmg/environment.py
@@ -1,4 +1,7 @@
-class Environment(object):
+from zohmg.utils import fail
+
+
+class Environ(object):
     def __init__(self):
         self.environ = {}
         self.read_environ()
@@ -7,12 +10,12 @@ class Environment(object):
         return self.environ[key]
 
     def read_environ(self):
-        sys.path.append("") # add cwd
+        sys.path.append("") # add cwd so we can import from it.
         try:
             env = __import__("config/environment")
-        except ImportError:
-            print >> sys.stderr,"E: Could not import config/environment.py, does it exist?"
-            exit(1)
+        except ImportError, ioe:
+            msg = "E: Could not import config/environment.py. %s" % ioe.strerror
+            fail(msg,ioe.errno)
 
         for key in dir(env):
             self.environ[key] = env.__dict__[key]
