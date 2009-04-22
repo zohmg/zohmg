97f24c41386228487fdc599ce42a76a1f4fed5e8
Per Andersson <per@last.fm>
2009-04-22 16:30:13 +0100

    INSTALL: added initial content. TODO: some notes about eggs. re-install.sh: omitted a few obsolete things.

--- tmp/re-install.sh
+++ tmp/re-install.sh
@@ -7,6 +7,6 @@ sudo rm -v /usr/bin/zohmg /usr/lib/python2.5/site-packages/zohmg-*-py2.5.egg /us
 
 # reinstate.
 echo -n hmm..
-sudo python install.py  --verbose install > tmp/last.reinstall.log
+sudo python install.py > tmp/last.reinstall.log
 sudo rm -rf build dist zohmg.egg-info
 echo ok.
