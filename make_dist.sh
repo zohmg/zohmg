b9f7a6d93c02615230055da8aa2f51b8785baf77
Per Andersson <per@last.fm>
2009-04-23 12:30:14 +0100

    removed  in output.

--- make_dist.sh
+++ make_dist.sh
@@ -4,7 +4,7 @@ set -e
 
 # TODO: get version from somewhere
 DATE=$(date +%Y%m%d)
-GITCOMMIT="$(git log -1 | sed -n '/commit/ {s/commit //;p}')"
+GITCOMMIT="git$(git log -1 | sed -n '/commit/ {s/commit //;p}')"
 VERSION="alpha-$DATE-$GITCOMMIT"
 BUILD=zohmg-$VERSION
 DIST=dist
@@ -14,7 +14,7 @@ printf "Creating dist target..."
 mkdir -p $BUILD_TARGET
 echo "done."
 
-printf "Copying dist files to $BUILD_TARGET..."
+printf "Copying dist files to temporary target..."
 cp AUTHORS INSTALL QUICKSTART README TODO $BUILD_TARGET
 cp install.py setup.py $BUILD_TARGET
 cp -r examples java lib src tests zohmg $BUILD_TARGET
