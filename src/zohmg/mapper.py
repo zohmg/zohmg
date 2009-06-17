1e1d6d743a957eb9c43c5e9e0c638d78fd5c3e2c
Fredrik Mollerstrand <fredrik@mollerstrand.se>
2009-06-17 20:19:54 +0100

    mapper: added tests and plenty of comments.

--- src/zohmg/mapper.py
+++ src/zohmg/mapper.py
@@ -20,39 +20,66 @@ from zohmg.config import Config
 # exploding mapper!
 # every emitted record from the usermapper is expanded
 # to len(projections) * len(units) * 2^len(reduced) records.
+#
+# the usermapper yields tuples of the form (ts, dimensions, units).
+# this mapper wrapper turns them into tuples of the form:
+# ((ts, p, permut, unit), value).  er, read the code please.
 class Mapper(object):
-    def __init__(self, usermapper):
+    def __init__(self, usermapper, projections=None):
         self.usermapper = usermapper
-        self.projections = Config().projections()
+        if projections == None:
+            projections = Config().projections()
+        self.projections = projections
 
-    # helper for generating all permutations of a dictionary.
+    # helper for generating (per)mutations of a dictionary.
+    # returns a list of 2**len(input) number of dicts, like so:
+    # {'a':'x', 'b':'y'} => [{'a':'x', 'b':'y'}, {'a':'x', 'b':'all'},
+    #                        {'a':'all', 'b':'y'}, {'a':'all', 'b':'all'}]
     def dict_permutations(self, dict_):
         dict = dict_.copy()
 
         # base case.
         if len(dict) == 1:
-            # i.e., {'agent': 'firefox'} => [{'agent': 'firefox'}, {'agent': 'all'}]
-            return [dict, {dict.keys()[0] : 'all'}]
+            all_dict = {dict.keys()[0] : 'all'}
+            # dict     => {'agent': 'firefox'}
+            # all_dict => {'agent': 'all'}
+            return [dict, all_dict]
 
-        x  = dict.popitem()
-        xs = self.dict_permutations(dict)
 
+        # assume dict => {'agent' : 'firefox', 'country' : 'SE', 'http-status' : '200'}
+        x  = dict.popitem()               # x => ('agent', 'firefox')
+        xs = self.dict_permutations(dict) # xs => [{'http-status': '200', 'country': 'SE'}, {'http-status': '200', 'country': 'all'}, {'http-status': 'all', 'country': 'SE'}, {'http-status': 'all', 'country': 'all'}]
+
+        # combine x and xs into a list.
         permuts = []
-        for a in [x, (x[0], 'all')]:
-            for b in xs: # b => {'agent': "firefox", 'country': "US"}
-                b[a[0]] = a[1] # add a to b.
+        for k,v in [x, (x[0], 'all')]:
+            # k => 'agent'
+            # v => 'firefox' and 'all'
+            for b in xs:
+                # b => {'http-status': '200', 'country': 'SE'}
+                # add {'agent':'firefox'} or {'agent':'all'} to b.
+                b[k] = v
                 permuts.append(b.copy())
         return permuts
 
     # wrapper around the user's mapper.
     def __call__(self, key, value):
         # from the usermapper: a timestamp, a point in n-space, units and their values.
-        for (ts, point, units) in self.usermapper(key, value):
-            for p in self.projections:
+        for (timestamp, dimensions, units) in self.usermapper(key, value):
+            for projection in self.projections:
+                # for example, if projection => ('user', 'artist')
+                # and dimensions => {'user':100, 'artist':2002, 'track':822010}
+                # then reduced => {'user':100, 'artist':2002}
                 reduced = {}
-                for d in p:  # dimensionality reduction.
-                    reduced[d] = point[d]
-                for u in units:
-                    value = units[u]
-                    for permut in self.dict_permutations(reduced):
-                        yield (ts, p, permut, u), value
+                for d in projection:  # dimensionality reduction.
+                    reduced[d] = dimensions[d]
+                for unit in units:
+                    value = units[unit]
+                    permutations = self.dict_permutations(reduced)
+                    # if reduced => {'user':100, 'artist':2002}
+                    # then permutations => [{'user':100, 'artist':2002},
+                    #                        {'user':100, 'artist':'all'},
+                    #                        {'user':'all', 'artist':2002},
+                    #                        {'user':'all', 'artist':'all'}]
+                    for permut in permutations:
+                        yield (timestamp, projection, permut, unit), value
--- tests/test_mapper.py
+++ tests/test_mapper.py
@@ -18,21 +18,50 @@
 import unittest
 from zohmg.mapper import Mapper
 
-# simple usermapper.
+# void usermapper.
 def m(key, value):
     pass
 
-# how to mock Config.projections()?
-# http://python-mock.sourceforge.net/
-# or http://blog.ianbicking.org/minimock.html
+# mock of usermapper.
+def mock_mapper(k, v):
+    ymd = '20090618'
+    yield (ymd, {'agent':'firefox', 'path':'/about'}, {'hit':1})
 
 class TestMapper(unittest.TestCase):
     def test_dict_permutations(self):
-        mapper = Mapper(m) # setup.
-        
+        mapper = Mapper(m, []) # setup with void mapper and empty projection list.
+
         p = mapper.dict_permutations({'agent': 'firefox'})
         self.assertEqual(p, [{'agent': 'firefox'}, {'agent': 'all'}])
 
+        p = mapper.dict_permutations({'a':'x', 'b':'y'})
+        expected = [{'a':'x', 'b':'y'}, {'a':'x', 'b':'all'}, {'a':'all', 'b':'y'}, {'a':'all', 'b':'all'}]
+        self.assertEqual(p, expected)
+
+        p = mapper.dict_permutations({'agent' : 'firefox', 'country' : 'SE', 'http-status' : '200', 'path':'/'})
+        expected = [{'http-status': '200', 'path': '/', 'agent': 'firefox', 'country': 'SE'}, {'http-status': '200', 'path': '/', 'agent': 'all', 'country': 'SE'}, {'http-status': '200', 'path': 'all', 'agent': 'firefox', 'country': 'SE'}, {'http-status': '200', 'path': 'all', 'agent': 'all', 'country': 'SE'}, {'http-status': '200', 'path': '/', 'agent': 'firefox', 'country': 'all'}, {'http-status': '200', 'path': '/', 'agent': 'all', 'country': 'all'}, {'http-status': '200', 'path': 'all', 'agent': 'firefox', 'country': 'all'}, {'http-status': '200', 'path': 'all', 'agent': 'all', 'country': 'all'}, {'http-status': 'all', 'path': '/', 'agent': 'firefox', 'country': 'SE'}, {'http-status': 'all', 'path': '/', 'agent': 'all', 'country': 'SE'}, {'http-status': 'all', 'path': 'all', 'agent': 'firefox', 'country': 'SE'}, {'http-status': 'all', 'path': 'all', 'agent': 'all', 'country': 'SE'}, {'http-status': 'all', 'path': '/', 'agent': 'firefox', 'country': 'all'}, {'http-status': 'all', 'path': '/', 'agent': 'all', 'country': 'all'}, {'http-status': 'all', 'path': 'all', 'agent': 'firefox', 'country': 'all'}, {'http-status': 'all', 'path': 'all', 'agent': 'all', 'country': 'all'}]
+        self.assertEqual(p, expected)
+
+    def test_mapper(self):
+        mapper = Mapper(mock_mapper, [['agent']])
+        output = list(mapper(0, 'bogus value'))
+        expected  = []
+        expected += [(('20090618', ['agent'], {'agent': 'firefox'}, 'hit'), 1)]
+        expected += [(('20090618', ['agent'], {'agent': 'all'}, 'hit'), 1)]
+        self.assertEqual(output, expected)
+
+        mapper = Mapper(mock_mapper, [['agent','path'], ['path'], ['agent']])
+        output = list(mapper(0, 'bogus value'))
+        expected  = []
+        expected += [(('20090618', ['agent', 'path'], {'path': '/about', 'agent': 'firefox'}, 'hit'), 1)]
+        expected += [(('20090618', ['agent', 'path'], {'path': '/about', 'agent': 'all'}, 'hit'), 1)]
+        expected += [(('20090618', ['agent', 'path'], {'path': 'all', 'agent': 'firefox'}, 'hit'), 1)]
+        expected += [(('20090618', ['agent', 'path'], {'path': 'all', 'agent': 'all'}, 'hit'), 1)]
+        expected += [(('20090618', ['path'], {'path': '/about'}, 'hit'), 1)]
+        expected += [(('20090618', ['path'], {'path': 'all'}, 'hit'), 1)]
+        expected += [(('20090618', ['agent'], {'agent': 'firefox'}, 'hit'), 1)]
+        expected += [(('20090618', ['agent'], {'agent': 'all'}, 'hit'), 1)]
+        self.assertEqual(output, expected)
 
 if __name__ == "__main__":
     unittest.main()
