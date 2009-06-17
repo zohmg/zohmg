1689eef28dfd0eb6f213e49adf5dcf30dfa6e825
Fredrik Mollerstrand <fredrik@mollerstrand.se>
2009-06-17 14:35:00 +0100

    modded tests/fixture-generators/testdata-submissions.py - wip

--- tests/fixture-generators/testdata-submissions.py
+++ tests/fixture-generators/testdata-submissions.py
@@ -1,31 +1,17 @@
-# Licensed to the Apache Software Foundation (ASF) under one
-# or more contributor license agreements.  See the NOTICE file
-# distributed with this work for additional information
-# regarding copyright ownership.  The ASF licenses this file
-# to you under the Apache License, Version 2.0 (the
-# "License"); you may not use this file except in compliance
-# with the License.  You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing,
-# software distributed under the License is distributed on an
-# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-# KIND, either express or implied.  See the License for the
-# specific language governing permissions and limitations
-# under the License.
+#!/usr/bin/env python
+# these here programming codes are licensed under the gnu fearsome dude license.
 
-#!/usr/bin/python
 from random import random
+
+from hbase import Hbase
 from hbase.ttypes import *
 
+from thrift import Thrift
+from thrift.transport import TSocket
+from thrift.transport import TTransport
+from thrift.protocol import TBinaryProtocol
 
-def setup_transport(host):
-  from thrift import Thrift
-  from thrift.transport import TSocket
-  from thrift.transport import TTransport
-  from thrift.protocol import TBinaryProtocol
-  from hbase import Hbase
+def setup_thrift_transport(host):
   transport = TSocket.TSocket(host, 9090)
   transport = TTransport.TBufferedTransport(transport)
   protocol = TBinaryProtocol.TBinaryProtocol(transport)
@@ -34,50 +20,78 @@ def setup_transport(host):
   return client, transport
 
 
-# set up some test data. should be quick.
-
 table = 'submissions-test'
 
 units = ['scrobbles', 'loves']
-scaling = {'scrobbles': 1000, 'loves': 2}
+scaling = {'scrobbles': 100, 'loves': 2}
 
-users   = [120, 240, 360]
-artists = [1001, 1002, 1003, 1004, 1005]
+dimensions  = ['user', 'artist', 'track', 'album']
+projections = [('user'), ('artist', 'track'), ('user','artist','track','album')]
 
-projections = [('user'), ('user','artist')]
+year="2009"
+months = range(0, 12)
+days   = range(0, 30)
 
-hashes = {} # pre-compute hashes.
-for a in artists:
-    hashes[a] = hash(a) % 255
+
+# possible attribute values.
+attrs = {'users':   [120, 240, 360],
+         'artists': [1000, 1001, 1002, 1003, 1004, 1005, 1006, 1007, 1008],
+         'track':   range(0,512),
+         'album':   range(0,64)}
+
+
+
+def random_attribute_of(dimension):
+    try:
+        possible = attrs[dimension]
+    except:
+        print 'oh noes.'
+        return '0.'
+    r = int(random(len(possible)))
+    attr = possible[r]
+    return attr
+        
+
+def magic_precomputation():
+    # so as to not make the distribution entirely random.
+    hashes = {}
+    for dimension in attrs.keys():
+        hashes[dimension] = {}
+        for attr in attrs[dimension]:
+            hashes[dimension][attr] = hash(attr) % 255
+    return hashes
 
 def magic_computation(unit, dimension):
     return random() * hashes[dimension] * scaling[unit]
 
-
 def generate_data(client):
-    year="2009"
-    for month in range(1,13):
-        month = "%02d" % month
-        for day in range(1,31):
+    for month in months:
+        month = "%02d" % (month+1)
+        for day in days:
             ymd = year + month + "%02d" % day
-            for artist in artists:
+            for projection in projections:
                 rowkeyarray = []
-                rowkeyarray.append('artist')
-                rowkeyarray.append(str(artist))
-                rowkeyarray.append(ymd)
-                rowkey = '-'.join(rowkeyarray)
+                for dimension in projection:
+                    rowkeyarray.append(dimension)
+                    rowkeyarray.append(random_attribute_of(dimension)) # random.
+                    rowkeyarray.append(ymd)
+                    rowkey = '-'.join(rowkeyarray)
+                    print 'rocknroll, know what im sayin: ' + rowkey
 
-                mutations = []
-                for unit in units:
-                    m = {}
-                    m['column'] = "unit:" + unit
-                    m['value']  = str(int(magic_computation(unit, artist)))
-                    mutations.append(Mutation(m))
-                print rowkey + " +> " + str(len(mutations)) + " mutations."
-                client.mutateRow(table, rowkey, mutations)
+                    mutations = []
+                    for unit in units:
+                        m = {}
+                        m['column'] = "unit:" + unit
+                        m['value']  = str(int(magic_computation(unit, dimension)))
+                        mutations.append(Mutation(m))
+                    print rowkey + " +> " + str(len(mutations)) + " mutations."
+                    #client.mutateRow(table, rowkey, mutations)
 
 
 if __name__ == '__main__':
-    client, transport = setup_transport('localhost')
+    client, transport = setup_thrift_transport('localhost')
+
+    hashes = magic_precomputation()
     generate_data(client)
+
     transport.close()
