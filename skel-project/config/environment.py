11f8f682a387a6595b2aa7989b963e76add184ba
Fredrik Mollerstrand <fredrik@last.fm>
2009-06-23 13:24:21 +0000

    updated skel-project/config/environment.py for 0.20

--- skel-project/config/environment.py
+++ skel-project/config/environment.py
@@ -1,10 +1,10 @@
 # path to yr hadoop directory.
-HADOOP_HOME = '/opt/hadoop-0.19.1'
+HADOOP_HOME = '/opt/hadoop-0.20.0'
 
 # jars for hadoop, hadoop streaming and hbase
 # (you may need to 'ant package' in $HADOOP_HOME to build the streaming jar.)
 CLASSPATH = (
-  '/opt/hadoop-0.19.1/build/hadoop-0.19.2-dev-core.jar',
-  '/opt/hadoop-0.19.1/build/contrib/streaming/hadoop-0.19.2-dev-streaming.jar',
-  '/opt/hbase-0.19.3/build/hbase-0.19.3.jar'
+  '/opt/hadoop-0.20.0/hadoop-0.20.0-core.jar',
+  '/opt/hadoop-0.20.0/build/contrib/streaming/hadoop-0.20.0-streaming.jar',
+  '/opt/hbase-0.20.0-alpha/hbase-0.20.0-alpha.jar'
 )
