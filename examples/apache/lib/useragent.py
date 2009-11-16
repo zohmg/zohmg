b4b8f3eb59f215b56e73cddc1e1f116affa5ba24
Fredrik Mollerstrand <fredrik@mollerstrand.se>
2009-11-16 15:03:59 +0100

    UserAgent passes tests.

--- examples/apache/lib/useragent.py
+++ examples/apache/lib/useragent.py
@@ -1,10 +1,45 @@
+import re
+
 class UserAgent:
+    browsers = [("Mozilla.*MSIE ([^;]+);.*"           , 'ie'),
+                ("Opera/([^ ]+).*|.*Opera ([^ ;]+).*" , 'opera'),
+                (".*Firefox/([^ ,;/()']+).*"          , 'firefox'),
+                (".* Chrome/([^ ,;/()']+).*"          , 'chrome'),
+                (".*Safari/([^ ,)]+).*"               , 'safari'),
+                (".*AppleWebKit/([^ )]+).*"           , 'webkit'), # generic webkit
+                (".*rv:([^ ;)]+)\\) Gecko/\\d+.*"     , 'gecko')]  # generic gecko
+    crawlers = [("Mozilla.*Googlebot/.*", 'google'),
+                ("Mozilla.*Ask Jeeves/Teoma.*", 'askjeeves'),
+                ("Mozilla.*Yahoo! Slurp.*", 'yahooslurp'),
+                ("facebookplatform/.*", 'facebook'),
+                ("Baiduspider.*", 'baidu'),
+                ("msnbot.*", 'msn')]
+
     def __init__(self, user_agent_string):
         self.user_agent_string = user_agent_string
         # compile regexes.
+        self.browser_patterns = self.compile_patterns(self.browsers)
+        self.crawler_patterns = self.compile_patterns(self.crawlers)
+
     def classify(self):
-        pass
+        for pattern, agent in self.browser_patterns + self.crawler_patterns:
+            if pattern.search(self.user_agent_string): return agent
+        return 'other'
+
     def is_browser(self):
-        pass
-    def is_agent(self):
-        pass
+        for pattern, agent in self.browser_patterns:
+            if pattern.search(self.user_agent_string): return True
+        return False
+
+    def is_robot(self):
+        for pattern, agent in self.crawler_patterns:
+            if pattern.search(self.user_agent_string): return True
+        return False
+
+
+    def compile_patterns(self, patterns):
+        compiled = []
+        for p, i in patterns:
+            compiled.append((re.compile(p), i))
+        return compiled
+
