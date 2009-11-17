c1e92e8c0525439c7e8262ba9ca3825b921ff7e3
Fredrik Mollerstrand <fredrik@mollerstrand.se>
2009-11-17 11:37:30 +0100

    examples/apache: updated environment.py to use build versions of jars.

--- examples/apache/config/environment.py
+++ examples/apache/config/environment.py
@@ -4,7 +4,8 @@ HBASE_HOME  = '/usr/local/hbase'
 # jars for hadoop, hadoop streaming and hbase
 # (you may need to 'ant package' in $HADOOP_HOME to build the streaming jar.)
 CLASSPATH = (
-  HADOOP_HOME + '/' + 'hadoop-0.20.1-core.jar',
+#  HADOOP_HOME + '/' + 'hadoop-0.20.1-core.jar',
+  HADOOP_HOME + '/' + 'build/hadoop-0.20.2-dev-core.jar',
   HADOOP_HOME + '/' + 'build/contrib/streaming/hadoop-0.20.2-dev-streaming.jar',
-  HBASE_HOME  + '/' + 'hbase-0.20.1.jar'
+  HBASE_HOME  + '/' + 'build/hbase-0.20.1.jar'
 )
